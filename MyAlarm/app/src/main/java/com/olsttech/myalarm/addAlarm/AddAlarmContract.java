c0355817ffe2bbea5bcb33ed57fbb1de42842455
Adetunji Adeolu <adetunji.ols@gmail.com>
2018-09-10 03:57:53 +0000

    AddAlarmContract.java edited online with Bitbucket

--- MyAlarm/app/src/main/java/com/olsttech/myalarm/addAlarm/AddAlarmContract.java
+++ MyAlarm/app/src/main/java/com/olsttech/myalarm/addAlarm/AddAlarmContract.java
@@ -1,8 +1,24 @@
 package com.olsttech.myalarm.addAlarm;
 
 /**
- * Created by adetunji on 01/09/2018.
+ * Created by adetunji on 01/09/2018. interface AddAlarmContract
  */
 
 public interface AddAlarmContract {
+  interface View {
+   void showAddAlarm()
+  }
+  
+  interface Presenter{
+   void setRepeat();
+   void addAlarmLabel();
+   void setSound();
+   void setAlarmTime();
+   void saveAlarm(@NonNull Alarm alarm, String alarmId);
+  }
+  
+  interface OnViewClickListener{
+   
+  }
+
 }
