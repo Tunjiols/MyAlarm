a63eecbf00761e20409348df24e70c2ede5fc06f
Adetunji Adeolu <adetunji.ols@gmail.com>
2018-09-13 09:07:52 +0000

    ShowRepeatActivity.java edited online with Bitbucket

--- MyAlarm/app/src/main/java/com/olsttech/myalarm/ShowRepeatActivity.java
+++ MyAlarm/app/src/main/java/com/olsttech/myalarm/ShowRepeatActivity.java
@@ -109,32 +109,6 @@ public class ShowRepeatActivity extends AppCompatActivity implements ShowRepeatC
         }
     } 
     
-    private class DayModel{
-        private String mDay;
-        private boolean mChecked;
-        
-        public DayModel(String day, boolean checked){
-            this.mDay = day;
-            this.mChecked = checked;
-        }
-        
-        public String getDay(){
-            return mDay;
-        }
-        
-        public void setDay(String day){
-            this.mDay = day;
-        }
-        
-        public boolean getChecked(){
-            return mChecked;
-        }
-        
-        public void setChecked(boolean checked){
-            this.mChecked = checked;
-        }
-        
-    }
     
     interface DayClickListener{
         void onDayClicked(DayModel dayModel);
