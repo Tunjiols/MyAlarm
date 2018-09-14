d6990795cf6c21b06112b46d1ffd25d99f09a908
Adetunji Adeolu <adetunji.ols@gmail.com>
2018-09-14 01:13:05 +0000

    LabelPresenter.java created online with Bitbucket

---
+++ MyAlarm/app/src/main/java/com/olsttech/myalarm/uis/LabelPresenter.java
@@ -0,0 +1,19 @@
+package com.olsttech.myalarm.uis;
+
+import com.olsttech.myalarm.models.DayModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LabelPresenter implements LabelContract.Presenter{
+
+    private LabelContract.View mView;
+    public LabelPresenter(LabelContract.View view){
+        this.mView = view;
+    }
+    @Override
+    void loadlabelEditScreen(){
+        mView.loadView();
+    }
+}
+    
